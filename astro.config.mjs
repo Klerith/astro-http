8cc18886c35f923b969ec641f1994669141ebbef
Fernando Herrera <fernando.herrera85@gmail.com>
2024-06-20 13:44:23 -0400

    chore: Update npm dependencies and Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,12 @@ import sitemap from '@astrojs/sitemap';
 // import node from "@astrojs/node";
 import cloudflare from "@astrojs/cloudflare";
 
+import db from "@astrojs/db";
 
 // https://astro.build/config
-export default defineConfig( {
+export default defineConfig({
   site: 'https://example.com',
-  integrations: [ mdx(), sitemap() ],
+  integrations: [mdx(), sitemap(), db()],
   output: "hybrid",
   adapter: cloudflare()
-} );
+});
---
+++ db/seed.ts
@@ -0,0 +1,8 @@
+import { db } from 'astro:db';
+
+// https://astro.build/db/seed
+export default async function seed() {
+  // TODO
+
+  console.log('Seed executed');
+}
