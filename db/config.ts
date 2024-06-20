f29afc2721d46efa96e2ac9969746af15cd241a4
Fernando Herrera <fernando.herrera85@gmail.com>
2024-06-20 13:51:09 -0400

    chore: Add Clients table to database configuration and seed with initial data

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,17 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
+
+const Clients = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text(),
+    age: column.number(),
+    isActive: column.boolean(),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    Clients,
+  },
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,8 +1,14 @@
-import { db } from 'astro:db';
+import { Clients, db } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-  // TODO
+  await db.insert(Clients).values([
+    { id: 1, name: 'Kasim', age: 35, isActive: true },
+    { id: 2, name: 'Fernando', age: 25, isActive: true },
+    { id: 3, name: 'Carlos', age: 37, isActive: true },
+    { id: 4, name: 'Melissa', age: 25, isActive: false },
+    { id: 5, name: 'Emin', age: 15, isActive: true },
+  ]);
 
   console.log('Seed executed');
 }
