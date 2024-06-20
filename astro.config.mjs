b9f9bdfa670b258f8c4c6905069fc2847fdacf8e
Fernando Herrera <fernando.herrera85@gmail.com>
2024-06-20 09:53:27 -0400

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
+
 
 // https://astro.build/config
 export default defineConfig( {
   site: 'https://example.com',
   integrations: [ mdx(), sitemap() ],
-  output: "hybrid",
-  adapter: node( {
-    mode: "standalone"
-  } )
+  output: "server",
+  adapter: cloudflare()
 } );
