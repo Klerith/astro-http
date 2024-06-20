3795165a1a665ff2f1fb45f873e15c149aa848cc
Fernando Herrera <fernando.herrera85@gmail.com>
2024-06-20 08:44:38 -0400

    chore: Add support for retrieving individual blog posts in API

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,12 +1,33 @@
 import type { APIRoute } from 'astro';
-import { getCollection } from 'astro:content';
+import { getCollection, getEntry } from 'astro:content';
 
 export const prerender = false;
 
 export const GET: APIRoute = async ({ params, request }) => {
-  const posts = await getCollection('blog');
+  const url = new URL(request.url);
+  const slug = url.searchParams.get('slug');
 
-  console.log(request);
+  if (slug) {
+    const post = await getEntry('blog', slug);
+
+    if (post) {
+      return new Response(JSON.stringify(post), {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    return new Response(JSON.stringify({ msg: `Post ${slug} not found` }), {
+      status: 404,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  const posts = await getCollection('blog');
 
   return new Response(JSON.stringify(posts), {
     status: 200,
