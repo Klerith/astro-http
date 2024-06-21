0e601a25ac69ff471702410a4e3a18d4df2877ff
Fernando Herrera <fernando.herrera85@gmail.com>
2024-06-21 15:27:16 -0400

    chore: Update npm dependencies and Astro configuration

--- src/components/likes/LikeCounter.vue
+++ src/components/likes/LikeCounter.vue
@@ -1,18 +1,93 @@
 <template>
 
-  <button @click="likePost">
-    Like Counter
+  <div v-if="isLoading">
+    Loading...
+  </div>
+
+
+  <button v-else-if="likeCount === 0" @click="likePost">
+      Like this post
   </button>
+
+  <button v-else @click="likePost">
+      Likes 
+      <span>{{ likeCount }}</span>
+  </button>
+
+  
 
 </template>
 
 
 <script lang="ts" setup>
+import { ref, watch } from 'vue';
+import confetti from 'canvas-confetti';
+import debounce from 'lodash.debounce'
 
-const likePost =()=> {
-  console.log('+1 like');
+interface Props {
+  postId: string;
 }
 
+const props = defineProps<Props>();
+
+const likeCount = ref(0);
+const likeClicks = ref(0);
+const isLoading = ref(true);
+
+
+watch( likeCount, debounce(() =>{
+  
+
+  fetch(`/api/posts/likes/${ props.postId }`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ likes: likeClicks.value })
+  });
+
+  likeClicks.value = 0;
+
+
+
+}, 500 ))
+
+
+
+
+const likePost =()=> {
+
+  likeCount.value++
+  likeClicks.value++;
+
+
+  confetti({
+    particleCount: 100,
+    spread: 70,
+    origin: {
+      x: Math.random(),
+      y: Math.random() - 0.2
+    }
+  })
+
+}
+
+
+
+const getCurrentLikes = async() => {
+  const resp = await fetch(`/api/posts/likes/${ props.postId }`);
+  if ( !resp.ok ) return;
+
+  const data = await resp.json();
+  
+  likeCount.value = data.likes;
+  isLoading.value = false; 
+
+
+}
+
+
+getCurrentLikes();
 
 </script>
 
