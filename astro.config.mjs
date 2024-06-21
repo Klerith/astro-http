4a99ac13c1289e0788aaf73413d37d23c4cf2378
Fernando Herrera <fernando.herrera85@gmail.com>
2024-06-21 14:55:49 -0400

    chore: Update npm dependencies and Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,14 @@ import sitemap from '@astrojs/sitemap';
 
 // import node from "@astrojs/node";
 import cloudflare from "@astrojs/cloudflare";
-
 import db from "@astrojs/db";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), db()],
+  integrations: [mdx(), sitemap(), db(), vue()],
   output: "hybrid",
   adapter: cloudflare()
 });
---
+++ src/components/likes/LikeCounter.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <button @click="likePost">
+    Like Counter
+  </button>
+
+</template>
+
+
+<script lang="ts" setup>
+
+const likePost =()=> {
+  console.log('+1 like');
+}
+
+
+</script>
+
+
+<style scoped>
+button {
+  background-color: #5e51bc;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all 0.3s;
+}
+
+button:hover {
+  background-color: #4a3f9a;
+}
+</style>
