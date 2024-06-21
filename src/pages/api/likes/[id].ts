6a701f56eed8fdf456f08b4e38cfb616871c8ebb
Fernando Herrera <fernando.herrera85@gmail.com>
2024-06-21 14:37:25 -0400

    chore: Update API route to handle likes for posts

--- src/pages/api/likes/[id].ts
+++ src/pages/api/likes/[id].ts
@@ -30,3 +30,28 @@ export const GET: APIRoute = async ({ params, request }) => {
     },
   });
 };
+
+export const PUT: APIRoute = async ({ params, request }) => {
+  const postId = params.id ?? '';
+
+  const posts = await db.select().from(Posts).where(eq(Posts.id, postId));
+  const { likes = 0 } = await request.json();
+
+  if (posts.length === 0) {
+    const newPost = {
+      id: postId,
+      title: 'Post not found',
+      likes: 0,
+    };
+
+    await db.insert(Posts).values(newPost);
+    posts.push(newPost);
+  }
+
+  const post = posts.at(0)!;
+  post.likes = post.likes + likes;
+
+  await db.update(Posts).set(post).where(eq(Posts.id, postId));
+
+  return new Response('Ok!', { status: 200 });
+};
