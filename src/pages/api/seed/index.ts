0a61d2a873e09ba1bb45a1e1a213fffd4171c7f2
Fernando Herrera <fernando.herrera85@gmail.com>
2024-11-04 09:56:52 -0500

    feat: add Post model and seed API for clients and posts

---
+++ src/pages/api/seed/index.ts
@@ -0,0 +1,48 @@
+import type { APIRoute } from 'astro';
+import { getCollection } from 'astro:content';
+
+import { v4 as UUID } from 'uuid';
+
+import prisma from '@db';
+
+const clients = [
+  { id: UUID(), name: 'John Doe', age: 29, is_active: true },
+  { id: UUID(), name: 'Jane Smith', age: 34, is_active: false },
+  { id: UUID(), name: 'Michael Johnson', age: 45, is_active: true },
+  { id: UUID(), name: 'Emily Davis', age: 22, is_active: false },
+  { id: UUID(), name: 'William Brown', age: 31, is_active: true },
+  { id: UUID(), name: 'Sophia Wilson', age: 27, is_active: false },
+  { id: UUID(), name: 'James Garcia', age: 38, is_active: true },
+  { id: UUID(), name: 'Olivia Miller', age: 41, is_active: false },
+  { id: UUID(), name: 'Daniel Martinez', age: 36, is_active: true },
+  { id: UUID(), name: 'Ava Anderson', age: 25, is_active: false },
+];
+
+export const GET: APIRoute = async ({ params, request }) => {
+  // Borrar los usuarios
+  await prisma.client.deleteMany();
+
+  // Limpiar los posts
+  await prisma.post.deleteMany();
+
+  // Insertar usuarios
+  await prisma.client.createMany({
+    data: clients,
+  });
+
+  // Insertar posts
+
+  const posts = await getCollection('blog');
+
+  const postData = posts.map((post) => ({
+    id: post.id,
+    title: post.data.title,
+    likes: Math.round(Math.random() * 100),
+  }));
+
+  await prisma.post.createMany({
+    data: postData,
+  });
+
+  return new Response('Ok!', { status: 200 });
+};
