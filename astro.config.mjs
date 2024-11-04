74def028d035f006861601143d383c08acdee73b
Fernando Herrera <fernando.herrera85@gmail.com>
2024-11-04 12:22:43 -0500

    feat: update Astro configuration to use node adapter; add server settings and update package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,24 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // import node from "@astrojs/node";
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
+import node from '@astrojs/node';
 
 
 import vue from '@astrojs/vue';
 
 
+
+
 // https://astro.build/config
 export default defineConfig( {
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), vue()],
+  integrations: [ mdx(), sitemap(), vue() ],
   output: "hybrid",
-  adapter: cloudflare()
+  adapter: node( {
+    mode: 'standalone'
+  } ),
+  server: {
+    port: process.env.PORT || 3000
+  }
 } );
