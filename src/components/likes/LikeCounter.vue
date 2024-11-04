b530fafc9a2e27ab2e1fed75da84eb19a9a5f13f
Fernando Herrera <fernando.herrera85@gmail.com>
2024-11-04 10:51:36 -0500

    feat: add canvas-confetti for like animation in LikeCounter component; update package dependencies

--- src/components/likes/LikeCounter.vue
+++ src/components/likes/LikeCounter.vue
@@ -1,10 +1,52 @@
 <template>
-  <!-- <span>Loading...</span> -->
+  <span v-if="isLoading">Loading...</span>
 
-  <!-- <button>Like this post</button> -->
+  <button v-else-if="likeCount === 0" @click="likePost">Like this post</button>
 
-  <button>Likes 1243</button>
+  <button v-else @click="likePost">Likes {{ likeCount }}</button>
+
+  <span>{{ likeClicks }}</span>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+import confetti from 'canvas-confetti';
+
+interface Props {
+  postId: string;
+}
+
+const props = defineProps<Props>();
+
+const likeCount = ref(0);
+const likeClicks = ref(0);
+const isLoading = ref(true);
+
+const likePost = async () => {
+  console.log('like +1');
+
+  likeCount.value += 1;
+  likeClicks.value += 1;
+
+  confetti({
+    particleCount: 100,
+    spread: 70,
+    origin: {
+      x: Math.random(),
+      y: Math.random() - 0.2,
+    },
+  });
+};
+
+const getCurrentLikes = async () => {
+  console.log(`fetching likes ${props.postId}`);
+
+  isLoading.value = false;
+};
+
+getCurrentLikes();
+</script>
 
 <style scoped>
 button {
